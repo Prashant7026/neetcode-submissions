647e7e15e5d9785350ade6402c38571fa1669daa
Prashant Kumar Soni <91582978+Prashant7026@users.noreply.github.com>
2026-06-29 21:58:48 +0530

    Add: jump-game - submission-8

---
+++ Data Structures & Algorithms/jump-game/submission-8.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int maxJump = nums[0];
+
+        for(int i = 0; i < nums.size(); i++) {
+            if(i > maxJump)     return false;
+
+            maxJump = max(maxJump, nums[i] + i);
+        }
+
+        return true;
+    }
+};
